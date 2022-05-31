743c5eb103de7a35511487378ce2b44d9b1fe78a
David Bismut <david.bismut@gmail.com>
2022-05-31 13:37:53 +0200

    fix everything

--- packages/three/src/three.ts
+++ packages/three/src/three.ts
@@ -1,5 +1,4 @@
-import { invalidate, addEffect } from '@react-three/fiber'
-import { FrameLoop, Target } from '@animini/core'
+import { Target } from '@animini/core'
 import { Color, Euler } from 'three'
 import { color, euler } from './adapters'
 import { ElementType, ThreeAdapter, Values } from './types'
@@ -9,23 +8,13 @@ const ADAPTERS = new Map<any, ThreeAdapter>([
   [Euler, euler]
 ])
 
-export const loop = new FrameLoop()
-loop.tick = () => invalidate()
-
-if (addEffect) {
-  addEffect(() => {
-    loop.update()
-  })
-}
-
 const three: Target<ElementType, Values<ElementType>> = {
   getInitialValueAndAdapter(element, key) {
     const value = element[key]
     const constructor = value.__proto__.constructor
     const adapter = ADAPTERS.get(constructor)
     return [value, adapter]
-  },
-  loop
+  }
 }
 
 export default three
--- packages/three/src/useAniminiThree.ts
+++ packages/three/src/useAniminiThree.ts
@@ -1,8 +1,31 @@
-import { useAniminiCore, Config } from '@animini/core'
+import { useAniminiCore, Config, GlobalLoop } from '@animini/core'
+import { addEffect } from '@react-three/fiber'
 import type { Object3D } from 'three'
 import target from './three'
 import { ElementType, Values } from './types'
+import { useEffect } from 'react'
+
+let count = 0
+
+function setGlobalLoopOnDemand() {
+  let unsub: () => void | undefined
+  if (count++ === 0) {
+    GlobalLoop.onDemand = true
+    unsub = addEffect(() => {
+      GlobalLoop.update()
+    })
+  }
+  return () => {
+    if (--count === 0) {
+      GlobalLoop.onDemand = false
+      unsub?.()
+    }
+  }
+}
 
 export function useAnimini<Element extends ElementType = Object3D>(masterConfig?: Config) {
+  useEffect(() => {
+    return setGlobalLoopOnDemand()
+  }, [])
   return useAniminiCore<Element, Values<Element>>(target, undefined, masterConfig)
 }
